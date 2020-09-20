42b64bac3626a4af5adbfdcf6c7a8e8ffdc97fa1
Artem Grankin <agrankin@klta.net>
2020-09-20 11:38:29 +0200

    add simple servlets and servlet-api dependency

---
+++ redirect-vs-forward/src/main/java/RedirectServlet.java
@@ -0,0 +1,10 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RedirectServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect(req.getContextPath() + "/redirected");
+    }
+}
